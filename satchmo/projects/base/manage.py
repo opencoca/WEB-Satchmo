f1f2acbab5a881860277408db94183e6e63ed1be
Dinh Bowman <dinh.bowman@gmail.com>
2010-08-25 17:28:00 -0700

    Addresses the strange case where some users could not edit their own contact info but were able to edit someone else's.

---
+++ satchmo/projects/base/manage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+try:
+    from pkg_resources import require
+except ImportError:
+    pass
+else:
+    require('django >=1.2, <=1.2.1')
+
+from django.core.management import execute_manager
+try:
+    import settings # Assumed to be in the same directory.
+except ImportError:
+    import sys
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    execute_manager(settings)
