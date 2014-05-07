612db493a60d808cdaa453bcd5e0e5e958d35fb6
Chris Moffitt <chris@moffitts.net>
2014-05-06 20:54:32 -0500

    Minor doc changes.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Satchmo'
-copyright = u'2012, Chris Moffitt'
+copyright = u'2014, Chris Moffitt'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
