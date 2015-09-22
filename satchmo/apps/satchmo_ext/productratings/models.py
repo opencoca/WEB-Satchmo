e1670ebcebf2299457ed67646b8e27a3280d4130
predatell <predatell@localhost>
2015-09-23 00:16:59 +0300

    check for django_comments

--- satchmo/apps/satchmo_ext/productratings/models.py
+++ satchmo/apps/satchmo_ext/productratings/models.py
@@ -1,9 +1,10 @@
-try:
-    from django.contrib.comments.models import Comment
-except ImportError:
-    from django_comments.models import Comment
+from django.conf import settings
 from django.db import models
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
+if 'django_comments' in settings.INSTALLED_APPS:
+    from django.contrib.comments.models import Comment
+else:
+    from django_comments.models import Comment
 from satchmo_utils.signals import collect_urls
 import product
 import satchmo_store
@@ -13,7 +14,6 @@ class ProductRating(models.Model):
     comment = models.OneToOneField(Comment, verbose_name="Rating", primary_key=True)
     rating = models.IntegerField(_("Rating"))
 
-import config
 from urls import add_product_urls, add_comment_urls
 collect_urls.connect(add_product_urls, sender=product)
 collect_urls.connect(add_comment_urls, sender=satchmo_store)
