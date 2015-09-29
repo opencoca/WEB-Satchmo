c6b5876d31b750fbcab2fcb4fe09bfc7e8e39fc7
predatell <predatell@localhost>
2015-09-29 15:14:19 +0300

    fix migrations

--- satchmo/apps/payment/migrations/0001_initial.py
+++ satchmo/apps/payment/migrations/0001_initial.py
@@ -35,7 +35,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('description', models.CharField(max_length=20, verbose_name='Description')),
-                ('active', models.BooleanField(help_text='Should this be displayed as an option for the user?', verbose_name='Active')),
+                ('active', models.BooleanField(default=False, help_text='Should this be displayed as an option for the user?', verbose_name='Active')),
                 ('optionName', models.CharField(help_text='The class name as defined in payment.py', unique=True, max_length=20, verbose_name='Option Name', choices=[('AUTHORIZENET', 'Credit Cards'), ('GIFTCERTIFICATE', 'Gift Certificate'), ('DUMMY', 'Payment test module')])),
                 ('sortOrder', models.IntegerField(verbose_name='Sort Order')),
             ],
