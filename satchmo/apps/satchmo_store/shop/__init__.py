cc7159f261c1799c6ad269bc08de41b3132dfaee
predatell <predatell@localhost>
2017-01-05 17:11:34 +0200

    AppConfig for shop app

--- satchmo/apps/satchmo_store/shop/__init__.py
+++ satchmo/apps/satchmo_store/shop/__init__.py
@@ -1,8 +1,3 @@
+default_app_config = 'satchmo_store.shop.apps.ShopConfig'
+
 from satchmo_settings import get_satchmo_setting
-import logging
-
-log = logging.getLogger('satchmo_store.shop')
-
-if get_satchmo_setting('MULTISHOP'):
-    log.debug('patching for multishop')
-    from threaded_multihost import multihost_patch
