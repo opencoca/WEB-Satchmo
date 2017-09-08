2cf009a079cd33b5c800a9f4278735ccf8926078
predatell <predatell@localhost>
2017-09-08 20:57:05 +0300

    Password widgets for codes

--- satchmo/apps/payment/forms.py
+++ satchmo/apps/payment/forms.py
@@ -446,10 +446,10 @@ class SimplePayShipForm(forms.Form):
 
 class CreditPayShipForm(SimplePayShipForm):
     credit_type = forms.ChoiceField()
-    credit_number = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'autocomplete':'off'}))
+    credit_number = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'autocomplete':'off'}))
     month_expires = forms.ChoiceField(choices=MONTHS)
     year_expires = forms.ChoiceField()
-    ccv = forms.CharField(max_length=4, label='Sec code', widget=forms.TextInput(attrs={'autocomplete':'off'}))
+    ccv = forms.CharField(max_length=4, label='Sec code', widget=forms.PasswordInput(attrs={'autocomplete':'off'}))
 
     def __init__(self, request, paymentmodule, *args, **kwargs):
         creditchoices = paymentmodule.CREDITCHOICES.choice_values
